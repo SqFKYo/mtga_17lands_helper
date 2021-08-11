a5ce3760480a342192eb14020af520d43d51df74
SqFKYo <sqfkyo@iki.fi>
2021-08-11 13:07:35 +0300

    Fixed crashing issue with QuickDraft

--- draft_helper.py
+++ draft_helper.py
@@ -8,7 +8,9 @@ from time import sleep
 import requests
 
 # AKR ChordOCalls
-DATA_SOURCE = r'https://www.17lands.com/card_tiers/data/638b3c8483804afa878db3b7edc638f8'
+# DATA_SOURCE = r'https://www.17lands.com/card_tiers/data/638b3c8483804afa878db3b7edc638f8'
+# KHM ChordOCalls
+DATA_SOURCE = r'https://www.17lands.com/card_tiers/data/69638dc98022443a9716300958e5fe9f'
 PLAYER_LOG = r'C:\Users\sqfky\AppData\LocalLow\Wizards Of The Coast\MTGA\Player.log'
 VALUE_MAP = {
     'A+': 10,
@@ -40,11 +42,15 @@ class DraftHelper:
         """Gets the most recent list of card_ids available in a draft"""
         if b"Draft.Notify" in line:
             card_ids = json.loads(line.split(b"Draft.Notify")[1])["PackCards"]
-        elif b"Draft.DraftStatus" in line:
-            card_ids = json.loads(line.split(b'Draft.DraftStatus')[1])["payload"]["DraftPack"]
+            cards = [self.tiers[int(card_id)] for card_id in card_ids.split(',')]
+        elif b"Draft.DraftStatus" in line and b"payload" in line:
+            head, tail = line.split(b'Draft.DraftStatus')
+            jsonified = json.loads(tail)
+            card_ids = jsonified["payload"]["DraftPack"]
+            cards = [self.tiers[int(card_id)] for card_id in card_ids]
         else:
             return
-        cards = [self.tiers[int(card_id)] for card_id in card_ids.split(',')]
+
         pick_order = sorted(cards, key=attrgetter('tier'), reverse=True)
         print("*"*36)
         for card in pick_order:
@@ -68,8 +74,12 @@ if __name__ == '__main__':
 
     with open(PLAYER_LOG, 'rb') as f:
         f.seek(0, 2)
+        print("Helper online, waiting for draft data.")
         while True:
             line = f.readline()
             if line:
                 helper.get_draft_choices(line)
             sleep(0.5)
+
+    # line = b'[UnityCrossThreadLogger]<== Draft.DraftStatus {"id":3490,"payload":{"DraftId":"LFZDDWQMAZC6BPN34NILOYZWNA:QuickDraft_KHM_20210810:Draft","DraftStatus":"Draft.PickNext","PackNumber":0,"PickNumber":0,"DraftPack":["75068","75071","75117","75202","75142","75199","75209","75159","75192","75046","75187","75114","75150","75047","75335"],"PickedCards":[]}}'
+    # helper.get_draft_choices(line)
